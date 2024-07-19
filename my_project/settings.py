f7169874bce3b1e4ff751a965c6861c9dd7d5129
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-07-19 08:30:11 +0000

    project ready to be deployed

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6^jnud8r=)dd2h(*e9$p74t$wj9h(f=+sy^_x#)khvbd4=g-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-aofficial0-djangoprojec-s3k5kzngpdu.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-aofficial0-djangoprojec-s3k5kzngpdu.ws.codeinstitute-ide.net',
+'.herokuapp.com']
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
